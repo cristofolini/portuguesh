9314d83a6564ad158dd897ef0fb1c183673abe65
Lucas Berri Cristofolini <lucascristofolini@gmail.com>
2018-10-20 17:01:53 -0300

    Adiciona a funcionalidade de funções à gramática

--- src/test/java/br/org/cristofolini/PortugueshTest.java
+++ src/test/java/br/org/cristofolini/PortugueshTest.java
@@ -1,11 +1,16 @@
 package br.org.cristofolini;
 
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+
+import javax.swing.*;
+import java.util.Arrays;
 
 public class PortugueshTest
 {
@@ -13,16 +18,28 @@ public class PortugueshTest
 
     @Test
     public void testExemplo() throws Exception {
-        PortugueshLexer l = new PortugueshLexer(new ANTLRInputStream(getClass().getResourceAsStream("/exemplo.psh")));
+        PortugueshLexer l = new PortugueshLexer(new ANTLRInputStream(getClass().getResourceAsStream("/exemplo2.psh")));
         PortugueshParser p = new PortugueshParser(new CommonTokenStream(l));
 
         p.addErrorListener(PortugueshErrorListener.INSTANCIA);
         p.addParseListener(new PortugueshParseTreeListener());
         p.setBuildParseTree(true);
-        RuleContext tree = p.prog();
-        System.out.println(tree.toStringTree(p));
-        logger.log(Level.forName("PARSE TREE", 375), tree.toStringTree(p));
+        ParseTree tree = p.prog();
+        //System.out.println(tree.toStringTree(p));
+        //logger.log(Level.forName("PARSE TREE", 375), tree.toStringTree(p));
         if (PortugueshErrorListener.INSTANCIA.dirtyBit) throw new ParseCancellationException();
+
+        JFrame frame = new JFrame("Antlr AST");
+        JPanel panel = new JPanel();
+        TreeViewer viewr = new TreeViewer(Arrays.asList(
+                p.getRuleNames()),tree);
+        viewr.setScale(1.5);//scale a little
+        panel.add(viewr);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(200,200);
+        frame.setVisible(true);
+        logger.log(Level.forName("PARSE TREE", 375), tree.toStringTree(p));
     }
 
     @Test
